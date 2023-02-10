3f881179c04ac2101295182afbd586e21eb5a99a
Dinh The Loc <locdtptit@gmail.com>
2023-02-10 12:46:27 +0700

    init project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,55 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { CenteredContentLayoutComponent } from './layouts/centered-content-layout/centered-content-layout.component'
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component'
+import { HomeComponent } from './pages/home/home.component'
+import { LoginComponent } from './pages/login/login.component'
+import { RegisterComponent } from './pages/register/register.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: [{ path: 'home', component: HomeComponent }],
+    },
+    {
+        path: '',
+        component: CenteredContentLayoutComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent },
+        ],
+    },
+    {
+        path: 'home',
+        loadComponent: () =>
+            import('./pages/home/home.component').then(
+                (mod) => mod.HomeComponent
+            ),
+    },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./pages/login/login.component').then(
+                (mod) => mod.LoginComponent
+            ),
+    },
+    {
+        path: 'register',
+        loadComponent: () =>
+            import('./pages/register/register.component').then(
+                (mod) => mod.RegisterComponent
+            ),
+    },
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { LayoutsModule } from './layouts/layouts/layouts.module'
+import { HomeComponent } from './pages/home/home.component'
+import { LoginComponent } from './pages/login/login.component'
+import { RegisterComponent } from './pages/register/register.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+
+        LayoutsModule,
+
+        HomeComponent,
+        LoginComponent,
+        RegisterComponent,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
