8f84ac915a929c91e4c19f415ee414d1acf35333
Dinh The Loc <locdtptit@gmail.com>
2023-03-21 14:23:09 +0700

    update v16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,14 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { LoaderComponent } from './components/loader/loader.component'
+import { AuthInterceptor } from './interceptors/auth.interceptor'
+import { LoadingInterceptor } from './interceptors/loading.interceptor'
 import { LayoutsModule } from './layouts/layouts/layouts.module'
 import { HomeComponent } from './pages/home/home.component'
 import { LoginComponent } from './pages/login/login.component'
 import { RegisterComponent } from './pages/register/register.component'
 import { CrudService } from './services/crud.service'
-import { IndexComponent } from './pages/index/index.component'
-import { LoaderComponent } from './components/loader/loader.component'
-import { LoadingInterceptor } from './interceptors/loading.interceptor'
-import { AuthInterceptor } from './interceptors/auth.interceptor'
-import { TestService } from './services/test.service'
 
 @NgModule({
     declarations: [AppComponent, LoaderComponent],
@@ -21,9 +19,7 @@ import { TestService } from './services/test.service'
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
-
         LayoutsModule,
-
         HomeComponent,
         LoginComponent,
         RegisterComponent,
