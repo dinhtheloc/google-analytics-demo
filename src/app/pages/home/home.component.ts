ec01f1d469fa71cf3a1a3e98d129e0f03b66cdec
SGVF\dinh.the-loc <dinh.the-loc@hdsaison.com.vn>
2023-03-30 10:36:41 +0700

    add google analytics

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
 import { RouterLink } from '@angular/router'
+import { RecaptchaV3Module, ReCaptchaV3Service, RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha'
 import { QuicklinkDirective } from 'ngx-quicklink'
 
 @Component({
     standalone: true,
-    imports: [RouterLink, QuicklinkDirective],
-    selector: 'app-home',
+    imports: [RouterLink, QuicklinkDirective, RecaptchaV3Module],
+     selector: 'app-home',
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
+    styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {}
+
+export class HomeComponent {
+    private recaptchaV3Service = inject(ReCaptchaV3Service)
+
+    public executeImportantAction(): void {
+        this.recaptchaV3Service.execute('importantAction')
+          .subscribe(console.log);
+      }
+    
+}
--- src/app/pages/home/home.routes.ts
+++ src/app/pages/home/home.routes.ts
@@ -1,6 +1,17 @@
-import { Routes } from "@angular/router";
-import { HomeComponent } from "./home.component";
+import { Routes } from '@angular/router'
+import { RECAPTCHA_V3_SITE_KEY } from 'ng-recaptcha'
+import { HomeComponent } from './home.component'
 
-export const homeRoutes: Routes = [{
-    path: '', component: HomeComponent
-}]
+export const homeRoutes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+
+        providers: [
+            {
+                provide: RECAPTCHA_V3_SITE_KEY,
+                useValue: '6Lc8IDUlAAAAANQ92hpn3mAhg9VGdcV9AdH-r0ON',
+            },
+        ],
+    },
+]
