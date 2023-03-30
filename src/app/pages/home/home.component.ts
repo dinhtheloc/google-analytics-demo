cdbd04ac3b1b8009d01702318b6c435aaf471854
SGVF\dinh.the-loc <dinh.the-loc@hdsaison.com.vn>
2023-03-30 17:19:30 +0700

    update demo google analytics

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,33 +1,67 @@
+import { NgClass, NgIf } from '@angular/common'
 import { Component, inject } from '@angular/core'
-import { RouterLink } from '@angular/router'
 import {
-    RecaptchaV3Module,
-    ReCaptchaV3Service,
-    RECAPTCHA_V3_SITE_KEY,
-} from 'ng-recaptcha'
+    AbstractControl,
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    ValidatorFn,
+    Validators,
+} from '@angular/forms'
+import { RouterLink } from '@angular/router'
+import { RecaptchaV3Module, ReCaptchaV3Service } from 'ng-recaptcha'
 import { QuicklinkDirective } from 'ngx-quicklink'
 import { GoogleAnalyticsService } from 'src/app/services/google-analytics.service'
 
+export function PhoneNumberValidator(): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+        const regexPhoneNumber = /(84|0[3|5|7|8|9])+([0-9]{8})\b/g
+        const phone = control.value
+        return phone.match(regexPhoneNumber)
+            ? null
+            : { wrongNumber: { value: phone } }
+    }
+}
+
 @Component({
     standalone: true,
-    imports: [RouterLink, QuicklinkDirective, RecaptchaV3Module],
+    imports: [
+        NgIf,
+        NgClass,
+        RouterLink,
+        QuicklinkDirective,
+        RecaptchaV3Module,
+        ReactiveFormsModule,
+    ],
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+    form: FormGroup = new FormGroup({
+        phoneNumber: new FormControl('', [
+            Validators.required,
+            PhoneNumberValidator(),
+        ]),
+    })
+
+    get f() {
+        return this.form.controls
+    }
+
     private recaptchaV3Service = inject(ReCaptchaV3Service)
 
     private googleAnalyticsService = inject(GoogleAnalyticsService)
     public executeImportantAction(): void {
-        // test trigger event
-        this.googleAnalyticsService.eventEmitter(
-            "intro.register",
-            {"value":"click","tags":{"phoneNumber":"0937100576"}}
-        )
-
         // this.recaptchaV3Service
         //     .execute('importantAction')
         //     .subscribe(console.log)
+    }
+
+    submit() {
+        this.googleAnalyticsService.eventEmitter('intro.register', {
+            value: 'click',
+            phoneNumber: this.form.value.phoneNumber,
+        })
     }
 }
